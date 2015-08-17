7b986697595d9bc0043280cb5f38a9626b670e0e
ldyyukito <ldyyukito@outlook.com>
2015-08-18 03:52:17 +0800

    add,but have a fatal bug

--- app.js
+++ app.js
@@ -40,7 +40,6 @@
 
  function formArray(tempArray) {
    var result = [];
-   //console.log(tempArray);
    tempArray.forEach(function(val) {
      for (var i = 0; i < result.length; i++) {
        if (val.student_id === result[i].id) {
@@ -48,7 +47,6 @@
          return;
        }
      }
-     //不能用数组做，数组最后没法让属性等于不同的科目；
      var obj = {};
      obj.id = val.student_id;
      obj.name = val.student_name;
@@ -60,113 +58,46 @@
 
  app.get('/scores', function(req, res) {
    connection.query('select * from student,score,subject where student.student_id = score.student_id and subject.subject_id = score.subject_id',
-    function(err, rows) {
-     var sortKey = req.query.sortKey;
-     var orderKey = req.query.orderKey;
-     console.log(rows);
-     array = formArray(rows);
-     res.send(getNewArray(sortKey, orderKey, array));
-     connection.end();
-   });
+     function(err, rows) {
+       var sortKey = req.query.sortKey;
+       var orderKey = req.query.orderKey;
+       array = formArray(rows);
+       res.send(getNewArray(sortKey, orderKey, array));
+       connection.end();
+     });
  });
 
 
  app.get('/delete', function(req, res) {
    var id = req.query.id;
-   console.log(id);
-   connection.query('delete from score where student_id ='+id,
-   function(err, rows) {
-     if (err) {
-       throw err;
-     }
-     res.send("true");
-     connection.end();
-   });
+   connection.query('delete from score where student_id =' + id,
+     function(err, rows) {
+       if (err) {
+         throw err;
+       }
+       res.send("true");
+       connection.end();
+     });
+ });
+
+ app.post('/add', function(req, res) {
+   var student = req.query.student;
+   console.log(student);
+   connection.query('insert into student values(' + student.id + ',' + student.name + ')',
+     function(err, rows) {
+       if (err) {
+         throw err;
+       }
+       connection.query('insert into score values(' + student.id + ',1,' + student.chinese + '),(' + student.id + ',2,' + studentmath + '),(' + id + ',3,' + student.english + ')',
+         function(err, rows) {
+           if (err) {
+             throw err;
+           }
+           connection.end();
+         });
+     });
  });
 
 
 
  app.listen(3000);
-
-
-
-
-
-
-
- //
- //
- // var getNewArray = require('./sort.js');
- // var express = require('express');
- // var mysql = require('mysql');
- //
- // var app = express();
- // var connection;
- //
- // var hbs = require('hbs');
- // app.set('view engine', 'html');
- // app.engine('html', hbs.__express);
- //
- // app.use(express.static('public'));
- // app.use(express.static('bower_components'));
- //
- // app.get('*', function(res, req, next) {
- //   connection = mysql.createConnection({
- //     host: 'localhost',
- //     user: 'root',
- //     password: '19940826',
- //     database: 'student_score'
- //   });
- //   connection.connect(function(err) {
- //     next();
- //   });
- // });
- //
- // var array = [];
- // app.get('/', function(req, res) {
- //   connection.query('select * from student,score,subject where student.student_id = score.student_id and subject.subject_id = score.subject_id',
- //     function(err, rows) {
- //       if (err) throw err;
- //       array = formArray(rows);
- //       res.render('index', {
- //         str: array
- //       });
- //       connection.end();
- //
- //     });
- // });
- //
- // function formArray(tempArray) {
- //   var result = [];
- //   //console.log(tempArray);
- //   tempArray.forEach(function(val) {
- //     for (var i = 0; i < result.length; i++) {
- //       if (val.student_id === result[i].id) {
- //         result[i][val.subject_name] = val.score;
- //         return;
- //       }
- //     }
- //     //不能用数组做，数组最后没法让属性等于不同的科目；
- //     var obj = {};
- //     obj.id = val.student_id;
- //     obj.name = val.student_name;
- //     obj[val.subject_name] = val.score;
- //     result.push(obj);
- //   });
- //   return result;
- // }
- //
- // app.get('/scores', function(req, res) {
- //   connection.query('select * from student,score,subject where student.student_id = score.student_id and subject.subject_id = score.subject_id',
- //    function(err, rows) {
- //     var sortKey = req.query.sortKey;
- //     var orderKey = req.query.orderKey;
- //     console.log(rows);
- //     array = formArray(rows);
- //     res.send(getNewArray(sortKey, orderKey, array));
- //     connection.end();
- //   });
- //
- // });
- //
- // app.listen(3000);
--- public/main.js
+++ public/main.js
@@ -2,12 +2,15 @@ $(function() {
   var order = $('thead th').data('order');
   $('thead').on('click', 'th', function() {
     var key = $(this).data('id');
-    $.get('/scores', {sortKey: key,orderKey: order}, function(scores) {
+    $.get('/scores', {
+      sortKey: key,
+      orderKey: order
+    }, function(scores) {
       $("tbody").empty();
       fill(scores);
     });
 
-    if ((key !== 'name') && (key !== 'do')&& (key !== 'id')) {
+    if ((key !== 'name') && (key !== 'do')) {
       if (order === "asc") {
         order = "dec";
       } else {
@@ -17,54 +20,119 @@ $(function() {
   });
 });
 
-$(function(){
-  $('.mmd').on('click', function() {
-  var key = $(this).data('id');
-  $.get('/delete', {id:key},function(resq) {
-   $('tbody #'+key).remove();
-    });
-  });
-});
 
 function fill(newResult) {
   var string = '';
   newResult.forEach(function(val) {
-    string += ('<tr>'+'<td>' + val.id + '</td>' + '<td>' + val.name + '</td>' + '<td>' + val.Chinese + '</td>' + '<td>' + val.Math +
-      '</td>' + '<td>' + val.English + '</td>' +'<td>'+ '删除>'+'</td>'+ '</tr>');
+    string += ('<tr>' + '<td>' + val.id + '</td>' + '<td>' + val.name + '</td>' + '<td>' + val.Chinese + '</td>' + '<td>' + val.Math +
+      '</td>' + '<td>' + val.English + '</td>' + '<td>' + '删除>' + '</td>' + '</tr>');
   });
   $('tbody').html(string);
 }
 
 
+$(function() {
+  $('.delete').on('click', function() {
+    var key = $(this).data('id');
+    $.get('/delete', {
+      id: key
+    }, function(resq) {
+      $('tbody #' + key).remove();
+    });
+  });
+});
 
-//
-//
-// $(function() {
-//   var order = $('thead th').data('order');
-//   $('thead').on('click', 'th', function() {
-//     var key = $(this).data('id');
-//   //  console.log(order);
-//     $.get('/scores', {sortKey: key,orderKey: order}, function(scores) {
-//       $("tbody").empty();
-//     //  console.log(scores);
-//       fill(scores);
-//     });
-//
-//     if ((key !== 'name') && (key !== 'do')&& (key !== 'id')) {
-//       if (order === "asc") {
-//         order = "dec";
-//       } else {
-//         order = "asc";
-//       }
-//     }
-//   });
-// });
-//
-// function fill(newResult) {
-//   var string = '';
-//   newResult.forEach(function(val) {
-//     string += ('<tr>'+'<td>' + val.id + '</td>' + '<td>' + val.name + '</td>' + '<td>' + val.Chinese + '</td>' + '<td>' + val.Math +
-//       '</td>' + '<td>' + val.English + '</td>' +'<td>'+ '<input type="button" value="删除">'+'</td>'+ '</tr>');
-//   });
-//   $('tbody').html(string);
-// }
+$(function() {
+
+  $("#id").focus(function() {
+    var txt_value = $(this).val();
+    if (txt_value === "请输入学号") {
+      $(this).val("");
+    }
+  });
+  $("#id").blur(function() {
+    var txt_value = $(this).val();
+    if (txt_value === "") {
+      $(this).val("请输入学号");
+    }
+  });
+  $("#name").focus(function() {
+    var txt_value = $(this).val();
+    if (txt_value === "请输入姓名") {
+      $(this).val("");
+    }
+  });
+  $("#name").blur(function() {
+    var txt_value = $(this).val();
+    if (txt_value === "") {
+      $(this).val("请输入姓名");
+    }
+  });
+  $("#math").focus(function() {
+    var txt_value = $(this).val();
+    if (txt_value === "请输数学成绩") {
+      $(this).val("");
+    }
+  });
+
+
+  $("#math").blur(function() {
+    var txt_value = $(this).val();
+    if (txt_value === "") {
+      $(this).val("请输数学成绩");
+    }
+  });
+
+  $("#chinese").focus(function() {
+    var txt_value = $(this).val();
+    if (txt_value === "请输语文成绩") {
+      $(this).val("");
+    }
+  });
+  $("#chinese").blur(function() {
+    var txt_value = $(this).val();
+    if (txt_value === "") {
+      $(this).val("请输语文成绩");
+    }
+  });
+
+  $("#english").focus(function() {
+    var txt_value = $(this).val();
+    if (txt_value === "请输英语成绩") {
+      $(this).val("");
+    }
+  });
+  $("#english").blur(function() {
+    var txt_value = $(this).val();
+    if (txt_value === "") {
+      $(this).val("请输英语成绩");
+    }
+  });
+});
+
+//见AJAX的用法
+$(function() {
+  $('#add').on('click', function() {
+    var student = {
+      id: $("#id").val(),
+      name: $("#name").val(),
+      chinese: $('#chinese').val(),
+      math: $('#math').val(),
+      english: $('#english').val()
+    };
+    $.ajax({
+      url: '/add',
+      data: student,
+      type: 'post',
+      dataType: 'text',
+      success: function() {
+        $("tbody").append("<tr id=" + studnt.id + ">" +
+          "<td>" + student.name + "</td>" +
+          "<td>" + student.chinese + "</td>" +
+          "<td>" + student.math + "</td>" +
+          "<td>" + student.english + "</td>" +
+          '<td>' + '删除>' + '</td>' + "</tr>");
+      }
+    });
+  });
+});
